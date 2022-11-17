5ff1dac291834aed1c88d9143d5f49f7a5265e29
archfiend-Tanvir <tanvirelhussain@gmail.com>
2022-11-17 19:53:25 +0000

    Update APP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import NavBAr from "./components/Navbar";
-// https://www.youtube.com/watch?v=VzWBLj_CfpE 4 min in
+// https://www.youtube.com/watch?v=VzWBLj_CfpE 15 min in
 
 function App() {
   return (
