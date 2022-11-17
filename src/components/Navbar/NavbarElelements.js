f7507def81e701823f4971f50eb5b02709b28c60
archfiend-Tanvir <tanvirelhussain@gmail.com>
2022-11-17 19:37:48 +0000

    Nav Added

--- src/components/Navbar/NavbarElelements.js
+++ src/components/Navbar/NavbarElelements.js
@@ -2,4 +2,9 @@ import styled from "styled-components";
 import { NavLink as Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 
-export const Nav = styled.nav``;
+export const Nav = styled.nav`
+  background: black;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+`;
