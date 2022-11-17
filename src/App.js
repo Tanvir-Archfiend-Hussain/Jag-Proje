989741d463aa4e076fb7b7ee26d3359d568d7f03
archfiend-Tanvir <tanvirelhussain@gmail.com>
2022-11-17 17:37:43 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import NavBAr from "./components/Navbar";
-
 // https://www.youtube.com/watch?v=VzWBLj_CfpE 4 min in
 
 function App() {
   return (
-    <div className="App">
+    <>
       <NavBAr />
-    </div>
+    </>
   );
 }
 
