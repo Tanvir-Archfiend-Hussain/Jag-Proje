dc22dc16b2fa4388e8bd3956ea83bfb996a6a220
archfiend-Tanvir <tanvirelhussain@gmail.com>
2022-11-18 13:36:40 +0000

    Update Style Component

--- src/components/Navbar/NavbarElelements.js
+++ src/components/Navbar/NavbarElelements.js
@@ -27,4 +27,14 @@ export const navLink = styled(Link)`
 export const Bars = styled(FaBars)`
   display: none;
   color: #fff;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
 `;
