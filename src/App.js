6a6f0cb9675109e5b94cddb36431893217ad5c02
archfiend-Tanvir <tanvirelhussain@gmail.com>
2022-11-18 14:10:09 +0000

    Updates Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import NavBAr from "./components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
 // https://www.youtube.com/watch?v=VzWBLj_CfpE 15 min in
 
 function App() {
